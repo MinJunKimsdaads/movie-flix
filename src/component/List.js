a8f12d65794bbf465b8f4d176289a9efb44a8fec
MinJunKimsdaads <aaeexx@naver.com>
2023-07-06 13:11:51 +0900

    fetch

--- src/component/List.js
+++ src/component/List.js
@@ -1,13 +1,21 @@
 import { useEffect } from "react";
+import axios from "axios";
 function List({menu}){
     
-    const fetchList = () => {
+    const fetchList = async() => {
         try{
             // const query = menu === 'all' ? '':`&category=${category}`;
+            const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=45c6a13c9f39865d3a3e9d48c9989352&language=en-US&page=1`,);
+            console.log(response);
         }catch(e){
             console.log(e);
         }
     }
+
+    useEffect(()=>{
+        fetchList();
+    })
+
     return (
         <div>
 
