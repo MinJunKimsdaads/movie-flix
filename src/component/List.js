f5699b6b2a3f4314b0d6b0ee69ed186b00bab21e
MinJunKimsdaads <aaeexx@naver.com>
2023-07-12 17:05:04 +0900

    api key 감추기

--- src/component/List.js
+++ src/component/List.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import axios from "axios";
 function List({menu}){
+
+    
     
     const fetchList = async() => {
         try{
             // const query = menu === 'all' ? '':`&category=${category}`;
-            const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=45c6a13c9f39865d3a3e9d48c9989352&language=en-US&page=1`,);
+            const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.React_APP_TMDB_API_KEY}&language=en-US&page=1`,);
             // console.log(response);
         }catch(e){
             console.log(e);
