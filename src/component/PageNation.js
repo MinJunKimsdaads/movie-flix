8250df921379a656a03f7c07c3437219a79aab8a
MinJunKimsdaads <aaeexx@naver.com>
2023-07-24 17:34:47 +0900

    pagenation

--- src/component/PageNation.js
+++ src/component/PageNation.js
@@ -1,7 +1,10 @@
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { moveToFirst, moveToEnd, moveToNext, moveToPrev } from "../store/Store";
 
 function PageNation({page, limit, total}){
+    const block = 5;
+    const [blockArr, setBlockArr] = useState([Math.trunc(page/block)*block+1,Math.trunc(page/block)*block+2,Math.trunc(page/block)*block+3,Math.trunc(page/block)*block+4,Math.trunc(page/block)*block+5]);
     const dispatch = useDispatch();
 
     const first = () => {
@@ -19,16 +22,18 @@ function PageNation({page, limit, total}){
     const prev = () => {
         dispatch(moveToPrev());
     }
+
+    useEffect(()=>{
+        setBlockArr([Math.trunc(page/block)*block+1,Math.trunc(page/block)*block+2,Math.trunc(page/block)*block+3,Math.trunc(page/block)*block+4,Math.trunc(page/block)*block+5]);
+        console.log(page);
+        console.log(blockArr);
+    },[page])
     
     return (
         <div>
             {page > 1 ? <span onClick={first}>처음</span>:null}
             {page > 1 ? <span onClick={prev}>이전</span>:null}
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
-            <span>5</span>
+            {blockArr.map((e)=>{return(<span key={e}>{e}</span>)})}
             {total > page ? <span onClick={next}>다음</span>:null}
             {total > page ? <span onClick={end}>끝</span>:null}
         </div>
