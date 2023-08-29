ef66ed49384eaf3215f9ac0a8afc99891f06d4de
MinJunKimsdaads <aaeexx@naver.com>
2023-08-29 12:09:59 +0900

    20230829

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,11 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { useParams,Link } from "react-router-dom";
 import { useQuery } from "react-query";
 import { genresList } from "../store/Store";
 import { selectGenre, unselectGenre } from "../store/Store";
 import styles from "../style/Nav.module.scss";
 
 function Nav(){
+    const {menu} = useParams(); //메뉴
     const genreArr = useSelector((state) => state.reducer2);
     const dispatch = useDispatch();
     
@@ -39,7 +40,7 @@ function Nav(){
     if(status === 'success'){
         return (
             <>
-                {navMenu.map(e => <div key={e.code} className={styles.navMenu}><span><Link to={`/${e.code}`}>{e.name}</Link></span></div>)}
+                {navMenu.map(e => <div key={e.code} className={menu === e.code ? styles.selectedNavMenu:styles.navMenu}><span><Link to={`/${e.code}`}>{e.name}</Link></span></div>)}
                 <div className={styles.category}>카테고리</div>
                 <div className={styles.categoryBtnBox}>
                     {status === 'success' ? data.map((e) => {if(genreArr.indexOf(Number(e.id)) > -1){return(<div key={e.id}><span id={e.id} key={e.id} onClick={removeGenre} className={styles.seletedGenre}>{e.name}</span></div>)}else{return(<div key={e.id}><span id={e.id} key={e.id} onClick={addGenre} className={styles.genre}>{e.name}</span></div>)};}) : null}
